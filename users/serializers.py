77e631b65518e65d36ee84cf806a4661060bf5be
Anastasia Tsiklauri <anastasia_tsiklauri@yahoo.com>
2025-03-23 23:57:25 +0400

    davaleba 20

--- users/serializers.py
+++ users/serializers.py
@@ -8,7 +8,6 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'phone_number', 'first_name', 'last_name']
         
-        
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
@@ -26,4 +25,19 @@ class RegisterSerializer(serializers.ModelSerializer):
         validated_data.pop('password2')
         user = User.objects.create_user(**validated_data)
         return user
+    
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'phone_number', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+        def update(self, instance, validated_data):
+            password = validated_data.pop('password', None)
+            if password:
+                instance.set_password(password)
+            return super().update(instance, validated_data)
     
--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 from rest_framework import mixins, viewsets
 from .models import User
-from .serializers import UserSerializer, RegisterSerializer
+from .serializers import UserSerializer, RegisterSerializer, UserProfileSerializer
 from rest_framework.permissions import IsAuthenticated
+from products.permissions import IsObjectOwnerReadOnly
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 class UserViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.all()
@@ -11,3 +14,19 @@ class UserViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.Gen
 class RegisterViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+
+class ProfileViewSet(viewsets.GenericViewSet, mixins.UpdateModelMixin, mixins.DestroyModelMixin, mixins.RetrieveModelMixin):
+    queryset = User.objects.all()
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated, IsObjectOwnerReadOnly]
+
+    def get_queryset(self):
+        return User.objects.filter(id=self.request.user.id)
+
+    @action(detail=False, methods=['get'], permission_classes=[IsAuthenticated])
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
+
+    def perform_destroy(self, instance):
+        instance.delete()
